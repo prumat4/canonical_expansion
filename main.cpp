dc8623252cd9e353a8423b3590b289caf9ccc694
maksymdidukh <maxym.didukh@logivations.com>
2024-03-05 21:06:41 +0200

    change type from int to uint64_t
    Signed-off-by: maksymdidukh <maxym.didukh@logivations.com>

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 #include <map>
 #include <algorithm>
 
-std::map<int, std::vector<int>> result_map;
+std::map<uint64_t, std::vector<uint64_t>> result_map;
 
 void precompute_result_map() {
-    std::vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+    std::vector<uint64_t> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 
-    for (int d : primes) {
+    for (uint64_t d : primes) {
         result_map[d] = {1};
         while (std::count(result_map.at(d).begin(), result_map.at(d).end(), result_map.at(d).back()) < 2) {
             result_map.at(d).push_back((result_map.at(d).back() * 2) % d);
@@ -21,16 +21,19 @@ void precompute_result_map() {
     }
 }
 
-int main() {
-    precompute_result_map();
-
+void print_result_map() {
     for (const auto& pair : result_map) {
         std::cout << pair.first << ": ";
-        for (int num : pair.second) {
+        for (uint64_t num : pair.second) {
             std::cout << num << " ";
         }
         std::cout << std::endl;
     }
+}
+
+int main() {
+    precompute_result_map();
+    print_result_map();
 
     return 0;
 }
