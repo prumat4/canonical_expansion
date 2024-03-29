43cfde7fa70ed7a33b960707e57e9a64efac7660
maksymdidukh <maxym.didukh@logivations.com>
2024-03-29 09:44:58 +0200

    add docker hub link, update report

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # canonical_expansion
 
+# Docker hub: 
+https://hub.docker.com/repository/docker/omega111111/factorization/general
 ## Build docker file
 ```bash
 1. docker build -t canonical_expansion .
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,30 @@
 #include <numeric>
 #include <random>
 #include "algorithms.h"
+#include <chrono>
+
+void testRhoPollardPerformance() {
+    std::random_device rd;
+    std::mt19937_64 e2(rd());
+    std::uniform_int_distribution<uint64_t> dist(std::llround(std::pow(2,10)), std::llround(std::pow(2,20)));
+
+    std::vector<double> executionTimes;
+    for (int i = 0; i < 100; ++i) {
+        uint64_t number = dist(e2);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        auto result = rhoPollard(number);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        executionTimes.push_back(elapsed.count());
+        std::cout << "Result for " << number << ": " << result << ", Time: " << elapsed.count() << " ms\n";
+    }
+
+    double averageTime = std::accumulate(executionTimes.begin(), executionTimes.end(), 0.0) / executionTimes.size();
+    std::cout << "Average execution time: " << averageTime << " ms\n";
+}
+
 
 int main(int argc, char** argv) {
     if (argc > 1) {
@@ -16,6 +40,8 @@ int main(int argc, char** argv) {
             uint64_t number = std::stoull(argv[1]);
             std::cout << "Processing number: " << number << std::endl;
             precomputeResultMap();
+
+            testRhoPollardPerformance();
 
             std::vector<uint64_t> factors = findCanonicalExpansion(number);
             std::cout << "Canonical expansion: ";
